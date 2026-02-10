b20a12b4d90ec7bb9e9b98e6a665ccf1e5e06335
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2026-02-10 18:04:59 +0530

    Admin service Setup

---
+++ services/admin-service-java/src/main/java/com/storks/adminservice/AdminServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.storks.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceJavaApplication.class, args);
+    }
+
+}
---
+++ services/admin-service-java/src/test/java/com/storks/adminservice/AdminServiceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storks.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
