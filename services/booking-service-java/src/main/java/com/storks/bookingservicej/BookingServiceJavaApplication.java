72547f828e74d25821bb080c87e192f9c9c7e18d
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2026-02-10 18:05:16 +0530

    Setup Booking Service

---
+++ services/booking-service-java/src/main/java/com/storks/bookingservicej/BookingServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.storks.bookingservicej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingServiceJavaApplication.class, args);
+    }
+
+}
---
+++ services/booking-service-java/src/test/java/com/storks/bookingservicej/BookingServiceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storks.bookingservicej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
