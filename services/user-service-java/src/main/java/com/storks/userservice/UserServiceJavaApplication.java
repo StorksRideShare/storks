66853c00bde044997d102d532e43b7166c6b16fc
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2026-02-10 18:08:20 +0530

    Created User Service

---
+++ services/user-service-java/src/main/java/com/storks/userservice/UserServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.storks.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceJavaApplication.class, args);
+    }
+
+}
