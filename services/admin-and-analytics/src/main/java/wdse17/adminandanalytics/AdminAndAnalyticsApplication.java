8e39dfbd88d079f8f1bf031ac5f03f3327f0f012
MovinduLochana <www.movindu@gmail.com>
2026-03-02 09:20:15 +0530

    Create services

---
+++ services/admin-and-analytics/src/main/java/wdse17/adminandanalytics/AdminAndAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package wdse17.adminandanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAndAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminAndAnalyticsApplication.class, args);
+	}
+
+}
---
+++ services/safty-and-verification/src/main/java/wdse17/saftyandverification/SaftyAndVerificationApplication.java
@@ -0,0 +1,13 @@
+package wdse17.saftyandverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaftyAndVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaftyAndVerificationApplication.class, args);
+	}
+
+}
