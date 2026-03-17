faf53be9d89197c17cca87e528f6394da9879f32
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2026-03-17 17:04:13 +0530

    Added docker and fixed some stuff

--- services/live-messaging/src/main/java/com/storks/livemessaging/LiveMessagingApplication.java
+++ services/live-messaging/src/main/java/com/storks/livemessaging/LiveMessagingApplication.java
@@ -1,7 +1,11 @@
 package com.storks.livemessaging;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LiveMessagingApplication {
@@ -10,4 +14,13 @@ public class LiveMessagingApplication {
         SpringApplication.run(LiveMessagingApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("*");
+            }
+        };
+    }
 }
