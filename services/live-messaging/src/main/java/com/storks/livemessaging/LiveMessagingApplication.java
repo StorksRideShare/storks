32647f0aff019d63084167c851dac22f0e557e70
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2026-03-17 12:54:20 +0530

    created Live Messaging using Spring

---
+++ services/live-messaging/src/main/java/com/storks/livemessaging/LiveMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.storks.livemessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveMessagingApplication.class, args);
+    }
+
+}
