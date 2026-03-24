65512b2fffb4a698c7dc5473e4464f718aa473b7
Lithika-binuwara <lithikabinu@gmail.com>
2026-03-24 12:47:41 +0530

    change properties file

--- services/admin-and-analytics/src/main/java/com/storks/entity/Child.java
+++ services/admin-and-analytics/src/main/java/com/storks/entity/Child.java
@@ -18,10 +18,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "children")
 @Data
+@Getter
+@Setter
 public class Child {
 
     @Id
